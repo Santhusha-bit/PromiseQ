a5777e20970a082cb01de1e881dfba8d311d4f54
Chamodi88 <chamodib88@gmail.com>
2021-03-16 09:05:47 +0100

    rm out folder´, change gitignore

--- homepage/out/_next/static/_L_SvzJJglQ8AxGhSi7Nk/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F6d55c9bacbf5cdd0ee6d.css","static\u002Fchunks\u002Fpages\u002Findex-d3466d5482ea45ccffbd.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b863a68ce17d12ff30ef.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
