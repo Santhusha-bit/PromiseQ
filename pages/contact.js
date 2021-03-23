7c010b3beefb33bff1b6d678bfcd85019a434ccd
Chamodi88 <chamodib88@gmail.com>
2021-03-23 16:51:44 +0100

    contact form added

---
+++ pages/contact.js
@@ -0,0 +1,16 @@
+import Meta from '../components/Meta'
+import ContactForm from '../components/ContactForm'
+
+const contact = () => {
+  
+  return (
+    <>
+    <Meta title='Contact Us'/>
+    <h2>Contact Us</h2>
+    <p>Do you have any questions? Please do not hesitate to contact us directly.</p>
+    <ContactForm />
+    </>
+  )
+}
+
+export default contact
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
-import Head from 'next/head'
-import homeStyles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title> promiseQ </title>
-      </Head>
-      <h1 className={homeStyles.title}> Hello Next ! </h1>
+      <h1> Hello Next ! </h1>
     </div>
   )
 }
