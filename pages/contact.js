189f909526eba22c3f6f33b3916e4983bd949551
anjula-sack <anjulashanaka@gmail.com>
2021-07-20 15:47:30 +0530

    Update the contact us form

--- pages/contact.js
+++ pages/contact.js
@@ -1,41 +1,45 @@
 import Meta from "components/Meta";
-import ContactForm from "components/ContactForm";
 import Header from "components/Header";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import { Box } from "@material-ui/core";
+import HubspotForm from "react-hubspot-form";
 
 const useStyles = makeStyles((theme) => ({
 	formContainer: {
-		marginLeft: theme.spacing(30),
-		marginRight: theme.spacing(30),
-		marginBottom: theme.spacing(6),
-		padding: theme.spacing(5),
-		[theme.breakpoints.down("sm")]: {
-			margin: theme.spacing(0),
-			padding: theme.spacing(2),
-		},
-	},
+		minHeight: '80vh',
+    marginLeft: theme.spacing(30),
+    marginRight: theme.spacing(30),
+    marginBottom: theme.spacing(6),
+    padding: theme.spacing(5),
+    [theme.breakpoints.down("sm")]: {
+      margin: theme.spacing(0),
+      padding: theme.spacing(2),
+    },
+  },
 }));
 
 const contact = () => {
-	const classes = useStyles();
-	return (
-		<>
-			<Meta title="Contact Us" />
-			<Header title="Contact Us" />
-			<Box alignItems="center" display="flex">
-				<Paper className={classes.formContainer}>
-					<Typography variant="subtitle1" gutterBottom>
-						Do you have any questions? Please do not hesitate to contact us
-						directly.
-					</Typography>
-					<ContactForm />
-				</Paper>
-			</Box>
-		</>
-	);
+  const classes = useStyles();
+  return (
+    <>
+      <Meta title="Contact Us" />
+      <Header title="Contact Us" />
+      <Paper className={classes.formContainer}>
+        <Typography variant="subtitle1" gutterBottom>
+          Do you have any questions? Please do not hesitate to contact us
+          directly.
+        </Typography>
+        <HubspotForm
+          portalId="9094398"
+          formId="219adb3f-b43c-4b02-babc-13467c28a98d"
+          onSubmit={() => console.log("SUCCESS")}
+          onReady={(form) => console.log("Form ready!")}
+          loading={<div>Loading...</div>}
+        />
+      </Paper>
+    </>
+  );
 };
 
 export default contact;
