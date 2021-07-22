8b6e5749b89cf43b206284bf85cc8073b1387e0c
anjula-sack <anjulashanaka@gmail.com>
2021-07-22 19:58:39 +0530

    Replace jobs with google sheets

--- pages/careers/[jobId].js
+++ pages/careers/[jobId].js
@@ -1,222 +1,286 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { CircularProgress, ListItemText } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import PrintIcon from "@material-ui/icons/Print";
-import ShareIcon from "@material-ui/icons/Share";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
-import Grid from "@material-ui/core/Grid";
+import PrintIcon from "@material-ui/icons/Print";
+import ShareIcon from "@material-ui/icons/Share";
 import Button from "components/Button";
-import {
-	FacebookShareButton,
-	FacebookIcon,
-	TwitterShareButton,
-	TwitterIcon,
-	WhatsappShareButton,
-	WhatsappIcon,
-	LinkedinShareButton,
-	LinkedinIcon,
-} from "next-share";
-import Popup from "reactjs-popup";
-import "reactjs-popup/dist/index.css";
-import { jobs } from "job_details";
 import Header from "components/Header";
 import Meta from "components/Meta";
-import { Link } from "@material-ui/core";
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+} from "next-share";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 const useStyles = makeStyles((theme) => ({
-	bold: {
-		fontWeight: "600",
-		marginTop: "5px",
-	},
-	listItems: {
-		padding: "0",
-	},
-	bullet: {
-		color: theme.palette.primary.main,
-	},
-	arrowBack: {
-		"&:hover": {
-			backgroundColor: "transparent",
-			color: "black",
-		},
-	},
-	link: {
-		TextDecoder: "none",
-	},
+  bold: {
+    fontWeight: "600",
+    marginTop: "5px",
+  },
+  listItems: {
+    padding: "0",
+  },
+  bullet: {
+    color: theme.palette.primary.main,
+  },
+  arrowBack: {
+    "&:hover": {
+      backgroundColor: "transparent",
+      color: "black",
+    },
+  },
+  link: {
+    TextDecoder: "none",
+  },
+  loading: {
+    minHeight: "80vh",
+  },
 }));
 
-export const getStaticPaths = async () => {
-	const paths = jobs.map((job) => ({
-		params: { jobId: job.jobId.toString() },
-	}));
-	return { paths, fallback: false };
-};
+const JobDescription = () => {
+  const router = useRouter();
+  const classes = useStyles();
 
-export const getStaticProps = async ({ params }) => {
-	const job = jobs.filter((job) => job.jobId.toString() === params.jobId);
-	return { props: { job: job[0] } };
-};
+  const [isLoading, setIsLoading] = useState(false);
+  const [jobs, setJobs] = useState({});
+  useEffect(() => getJobs(), []);
 
-const JobDescription = ({ job }) => {
-	const classes = useStyles();
+  const getJobs = () => {
+    setIsLoading(true);
+    axios
+      .get(
+        "https://script.googleusercontent.com/macros/echo?user_content_key=LgqDR_OI3V98GG0C882TWdCK4a_OLtx0f9Wv4V00Bize0rxglqNiYeJCENnogiU2nosjHayQENWtwosVSGglYr4HwWQLFR6fm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnKzCKFCumaUSK7ljkjex80I63qfXxmG6v-8r-3tw718FeMPQwoFpDjv416YetwoTCQgQAPRwLIGrM7dQ8ZiDohSb2wiOXKc1KNz9Jw9Md8uu&lib=MDYMs6LQBT0TIp2Gl7L0Z8tTEvnd06pZ6"
+      )
+      .then((result) => {
+        setIsLoading(false);
+        setJobs(result.data);
+      })
+      .catch();
+  };
 
-	return (
-		<>
-			<Grid>
-				<Meta title={job.jobTitle} />
-				<Header title={job.jobTitle} />
-				<Card elevation={0}>
-					<CardHeader
-						subheaderTypographyProps={{ variant: "subtitle1" }}
-						subheader={job.subTitle}
-					/>
-					<Grid>
-						<Link href="/careers" className={classes.link}>
-							<IconButton
-								className={classes.arrowBack}
-								aria-label="back"
-								disableRipple={true}
-							>
-								<Typography variant="body2">Go Back</Typography>
-							</IconButton>
-						</Link>
-					</Grid>
-					<CardActions disableSpacing>
-						<IconButton aria-label="print" onClick={() => window.print()}>
-							<PrintIcon />
-						</IconButton>
+  const job = jobs[router.query.jobId - 1];
+  return (
+    <>
+      {isLoading ? (
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          spacing={3}
+          className={classes.loading}
+        >
+          <CircularProgress color="primary" />
+        </Grid>
+      ) : (
+        <Grid>
+          <Meta title={job?.jobTitle} />
+          <Header title={job?.jobTitle} />
+          <Card elevation={0}>
+            <CardHeader
+              subheaderTypographyProps={{ variant: "subtitle1" }}
+              subheader={job?.subTitle}
+            />
+            <Grid onClick={() => router.back()}>
+              <IconButton
+                className={classes.arrowBack}
+                aria-label="back"
+                disableRipple={true}
+              >
+                <Typography variant="body2">Go Back</Typography>
+              </IconButton>
+            </Grid>
+            <CardActions disableSpacing>
+              <IconButton aria-label="print" onClick={() => window.print()}>
+                <PrintIcon />
+              </IconButton>
 
-						<Popup
-							trigger={
-								<IconButton>
-									<ShareIcon />
-								</IconButton>
-							}
-							position="right center"
-						>
-							<Typography variant="caption">
-								Share job advertisement on :
-							</Typography>
-							<Grid container spacing={2}>
-								<Grid item>
-									<LinkedinShareButton
-										url={`https://www.promiseq.com/careers/${job.jobId}`}
-										hashtag={"#promiseQ"}
-									>
-										<LinkedinIcon size={32} round />
-									</LinkedinShareButton>
-								</Grid>
-								<Grid item>
-									<WhatsappShareButton
-										url={`https://www.promiseq.com/careers/${job.jobId}`}
-										hashtag={"#promiseQ"}
-									>
-										<WhatsappIcon size={32} round />
-									</WhatsappShareButton>
-								</Grid>
-								<Grid item>
-									<FacebookShareButton
-										url={`https://www.promiseq.com/careers/${job.jobId}`}
-										hashtag={"#promiseQ"}
-									>
-										<FacebookIcon size={32} round />
-									</FacebookShareButton>
-								</Grid>
-								<Grid item>
-									<TwitterShareButton
-										url={`https://www.promiseq.com/careers/${job.jobId}`}
-										hashtag={"#promiseQ"}
-									>
-										<TwitterIcon size={32} round />
-									</TwitterShareButton>
-								</Grid>
-							</Grid>
-						</Popup>
-					</CardActions>
-					<CardContent>
-						<Typography className={classes.bold}>{job.aboutUsTitle}</Typography>
-						{job.aboutUs.map((paragraph) => (
-							<Typography paragraph key={paragraph.paraKey}>
-								{paragraph.para}
-							</Typography>
-						))}
-						<Typography className={classes.bold}>{job.salaryTitle}</Typography>
-						<Typography paragraph>{job.salaryText}</Typography>
-						<Typography className={classes.bold}>{job.taskTitle}</Typography>
-						<List>
-							{job.jobTasks.map((task) => (
-								<ListItem key={task.taskKey} className={classes.listItems}>
-									<ListItemIcon className={classes.bullet}>
-										<FiberManualRecordIcon />
-									</ListItemIcon>
-									<ListItemText primary={task.task} />
-								</ListItem>
-							))}
-						</List>
-						<Typography className={classes.bold}>
-							{job.qualificationTitle}
-						</Typography>
-						<List>
-							{job.jobQualifications.map((qualification) => (
-								<ListItem
-									key={qualification.qualificationKey}
-									className={classes.listItems}
-								>
-									<ListItemIcon className={classes.bullet}>
-										<FiberManualRecordIcon />
-									</ListItemIcon>
-									<ListItemText primary={qualification.qualification} />
-								</ListItem>
-							))}
-						</List>
-						<Typography className={classes.bold}>{job.othersTitle}</Typography>
-						<List>
-							{job.others.map((other) => (
-								<ListItem key={other.otherKey} className={classes.listItems}>
-									<ListItemIcon className={classes.bullet}>
-										<FiberManualRecordIcon />
-									</ListItemIcon>
-									<ListItemText primary={other.other} />
-								</ListItem>
-							))}
-						</List>
-						<Typography className={classes.bold}>{job.endText}</Typography>
-						<Typography className={classes.bold}>{job.inquiryText}</Typography>
-						<Grid container justify="center" spacing={8}>
-							{job.contactDetails.map((contact) => (
-								<Grid item key={contact.addressKey}>
-									{contact.address.map((line) => (
-										<Grid key={line.lineKey}>
-											<Typography variant="caption" color="inherit">
-												{line.line}
-											</Typography>
-										</Grid>
-									))}
-								</Grid>
-							))}
-						</Grid>
-						<Grid container justify="flex-end">
-							<Grid item>
-								<Button
-									id="apply button"
-									text="Apply"
-									href="mailto:application@promiseq.com"
-								/>
-							</Grid>
-						</Grid>
-					</CardContent>
-				</Card>
-			</Grid>
-		</>
-	);
+              <Popup
+                trigger={
+                  <IconButton>
+                    <ShareIcon />
+                  </IconButton>
+                }
+                position="right center"
+              >
+                <Typography variant="caption">
+                  Share job advertisement on :
+                </Typography>
+                <Grid container spacing={2}>
+                  <Grid item>
+                    <LinkedinShareButton
+                      url={`https://www.promiseq.com/careers/${job?.jobId}`}
+                      hashtag={"#promiseQ"}
+                    >
+                      <LinkedinIcon size={32} round />
+                    </LinkedinShareButton>
+                  </Grid>
+                  <Grid item>
+                    <WhatsappShareButton
+                      url={`https://www.promiseq.com/careers/${job?.jobId}`}
+                      hashtag={"#promiseQ"}
+                    >
+                      <WhatsappIcon size={32} round />
+                    </WhatsappShareButton>
+                  </Grid>
+                  <Grid item>
+                    <FacebookShareButton
+                      url={`https://www.promiseq.com/careers/${job?.jobId}`}
+                      hashtag={"#promiseQ"}
+                    >
+                      <FacebookIcon size={32} round />
+                    </FacebookShareButton>
+                  </Grid>
+                  <Grid item>
+                    <TwitterShareButton
+                      url={`https://www.promiseq.com/careers/${job?.jobId}`}
+                      hashtag={"#promiseQ"}
+                    >
+                      <TwitterIcon size={32} round />
+                    </TwitterShareButton>
+                  </Grid>
+                </Grid>
+              </Popup>
+            </CardActions>
+            <CardContent>
+              <Typography className={classes.bold}>About Us:</Typography>
+              {job?.aboutUs?.map((about) => (
+                <Typography paragraph>{about}</Typography>
+              ))}
+              <Typography className={classes.bold}>
+                {job?.salaryTitle}
+              </Typography>
+              <Typography paragraph>{job?.salaryText}</Typography>
+              <Typography className={classes.bold}>
+                These will be your tasks:
+              </Typography>
+              <List>
+                {job?.jobTasks?.map((task) => (
+                  <ListItem key={task.taskKey} className={classes.listItems}>
+                    <ListItemIcon className={classes.bullet}>
+                      <FiberManualRecordIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={task} />
+                  </ListItem>
+                ))}
+              </List>
+              <Typography className={classes.bold}>
+                You are offering these qualifications:
+              </Typography>
+              <List>
+                {job?.jobQualifications?.map((qualification) => (
+                  <ListItem
+                    key={qualification.qualificationKey}
+                    className={classes.listItems}
+                  >
+                    <ListItemIcon className={classes.bullet}>
+                      <FiberManualRecordIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={qualification} />
+                  </ListItem>
+                ))}
+              </List>
+              {job?.niceToHave?.length ? (
+                <>
+                  <Typography className={classes.bold}>
+                    Nice to have:
+                  </Typography>
+                  <List>
+                    {job?.niceToHave?.map((niceToHave, index) => (
+                      <ListItem key={index} className={classes.listItems}>
+                        <ListItemIcon className={classes.bullet}>
+                          <FiberManualRecordIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={niceToHave} />
+                      </ListItem>
+                    ))}
+                  </List>
+                </>
+              ) : null}
+              {job?.whyUs?.length ? (
+                <>
+                  <Typography className={classes.bold}>Why Us?</Typography>
+                  <List>
+                    {job?.whyUs?.map((us, index) => (
+                      <ListItem key={index} className={classes.listItems}>
+                        <ListItemIcon className={classes.bullet}>
+                          <FiberManualRecordIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={us} />
+                      </ListItem>
+                    ))}
+                  </List>
+                </>
+              ) : null}
+              <Typography className={classes.bold}>
+                We are looking forward to getting to know you!
+              </Typography>
+              <Typography className={classes.bold}>
+                Please drop us a note at:
+              </Typography>
+              <Grid container justify="center" spacing={8}>
+                <Grid item>
+                  <Grid>
+                    <Typography variant="caption" color="inherit">
+                      promiseQ GmbH i.G.
+                    </Typography>
+                  </Grid>
+                  <Grid>
+                    <Typography variant="caption" color="inherit">
+                      c/o The Drivery GmbH
+                    </Typography>
+                  </Grid>
+                  <Grid>
+                    <Typography variant="caption" color="inherit">
+                      Mariendorfer Damm 1
+                    </Typography>
+                  </Grid>
+                  <Grid>
+                    <Typography variant="caption" color="inherit">
+                      12099 Berlin
+                    </Typography>
+                  </Grid>
+                  <Grid>
+                    <Typography variant="caption" color="inherit">
+                      application@promiseq.com
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid container justify="flex-end">
+                <Grid item>
+                  <Button
+                    id="apply button"
+                    text="Apply"
+                    href="mailto:application@promiseq.com"
+                  />
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+      )}
+    </>
+  );
 };
 
 export default JobDescription;
--- pages/careers/index.js
+++ pages/careers/index.js
@@ -1,37 +1,49 @@
 import Meta from "components/Meta";
 import Header from "components/Header";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, CircularProgress } from "@material-ui/core";
 import JobList from "components/JobList";
-import { jobs } from "job_details";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export const getStaticProps = async () => {
-	return {
-		props: {
-			jobs,
-		},
-	};
-};
+const careers = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => getJobs(), []);
 
-const careers = ({ jobs }) => {
-	return (
-		<>
-			<Meta title="Careers" />
-			<Header title="Join Our Team" />
+  const getJobs = () => {
+    setIsLoading(true);
+    axios
+      .get(
+        "https://script.googleusercontent.com/macros/echo?user_content_key=LgqDR_OI3V98GG0C882TWdCK4a_OLtx0f9Wv4V00Bize0rxglqNiYeJCENnogiU2nosjHayQENWtwosVSGglYr4HwWQLFR6fm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnKzCKFCumaUSK7ljkjex80I63qfXxmG6v-8r-3tw718FeMPQwoFpDjv416YetwoTCQgQAPRwLIGrM7dQ8ZiDohSb2wiOXKc1KNz9Jw9Md8uu&lib=MDYMs6LQBT0TIp2Gl7L0Z8tTEvnd06pZ6"
+      )
+      .then((result) => {
+        setIsLoading(false);
+        setJobs(result.data);
+      })
+      .catch();
+  };
 
-			<section id="current_jobs" style={{height: "58vh"}}>
-				<Grid>
-					<Typography variant="h6" style={{marginBottom: "30px"}}>
-						Current job vacancies :
-					</Typography>
-				</Grid>
-				<Grid container justify="center" spacing={3}>
-					{jobs.map((job) => (
-						<JobList key={job.jobId} {...job} />
-					))}
-				</Grid>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <Meta title="Careers" />
+      <Header title="Join Our Team" />
+
+      <section id="current_jobs" style={{ height: "58vh" }}>
+        <Grid>
+          <Typography variant="h6" style={{ marginBottom: "30px" }}>
+            Current job vacancies :
+          </Typography>
+        </Grid>
+        <Grid container justify="center" spacing={3}>
+          {isLoading ? (
+            <CircularProgress color="primary" />
+          ) : (
+            jobs.map((job) => <JobList key={job.jobId} {...job} />)
+          )}
+        </Grid>
+      </section>
+    </>
+  );
 };
 
 export default careers;
