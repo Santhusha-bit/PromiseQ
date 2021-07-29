0c1bf622441b2b03c80a48754a405f06f8085d9c
anjula-sack <anjulashanaka@gmail.com>
2021-07-29 16:08:18 +0530

    Remove the second API call in careers page

--- components/JobList.js
+++ components/JobList.js
@@ -8,50 +8,50 @@ import Typography from "@material-ui/core/Typography";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 import WorkIcon from "@material-ui/icons/Work";
 import Grid from "@material-ui/core/Grid";
-import Link from "next/link";
 
 const useStyles = makeStyles((theme) => ({
-	cardAction: {
-		justifyContent: "flex-end",
-	},
-	avatar: {
-		backgroundColor: theme.palette.primary.main,
-		color: "black",
-		width: theme.spacing(8),
-		height: theme.spacing(8),
-	},
+  cardAction: {
+    justifyContent: "flex-end",
+  },
+  avatar: {
+    backgroundColor: theme.palette.primary.main,
+    color: "black",
+    width: theme.spacing(8),
+    height: theme.spacing(8),
+  },
 }));
 
-const JobList = ({ jobId, jobTitle, subTitle }) => {
-	const classes = useStyles();
+const JobList = ({ job, setCurrentJob }) => {
+  const classes = useStyles();
 
-	return (
-		<>
-			<Grid item xs={12} sm={12} md={8}>
-				<Card>
-					<CardHeader
-						avatar={
-							<Avatar aria-label="work" className={classes.avatar}>
-								<WorkIcon fontSize="large" />
-							</Avatar>
-						}
-						titleTypographyProps={{ variant: "h6" }}
-						title={jobTitle}
-						subheaderTypographyProps={{ variant: "subtitle1" }}
-						subheader={subTitle}
-					/>
-					<CardActions className={classes.cardAction}>
-						<Link href={`/careers/[jobId]`} as={`/careers/${jobId}`} passHref>
-							<IconButton className={classes.iconButton}>
-								<Typography>See More </Typography>
-								<NavigateNextIcon />
-							</IconButton>
-						</Link>
-					</CardActions>
-				</Card>
-			</Grid>
-		</>
-	);
+  return (
+    <>
+      <Grid item xs={12} sm={12} md={8}>
+        <Card>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="work" className={classes.avatar}>
+                <WorkIcon fontSize="large" />
+              </Avatar>
+            }
+            titleTypographyProps={{ variant: "h6" }}
+            title={job.jobTitle}
+            subheaderTypographyProps={{ variant: "subtitle1" }}
+            subheader={job.subTitle}
+          />
+          <CardActions className={classes.cardAction}>
+            <IconButton
+              className={classes.iconButton}
+              onClick={() => setCurrentJob(job)}
+            >
+              <Typography>See More </Typography>
+              <NavigateNextIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+      </Grid>
+    </>
+  );
 };
 
 export default JobList;
--- pages/careers.js
+++ pages/careers.js
@@ -1,13 +1,21 @@
 import Meta from "components/Meta";
 import Header from "components/Header";
-import { Grid, Typography, CircularProgress, Box, Container } from "@material-ui/core";
+import {
+  Grid,
+  Typography,
+  CircularProgress,
+  Box,
+  Container,
+} from "@material-ui/core";
 import JobList from "components/JobList";
+import JobDescription from "components/JobDescription";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 const careers = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [jobs, setJobs] = useState([]);
+  const [currentJob, setCurrentJob] = useState(null);
   useEffect(() => getJobs(), []);
 
   const getJobs = () => {
@@ -26,21 +34,33 @@ const careers = () => {
   return (
     <>
       <Container maxWidth="lg">
-        <Box marginTop={15}>
+        <Box marginTop={currentJob === null ? 15 : 5}>
           <Meta title="Careers" />
-          <Header title="Join Our Team" />
-
-          <section id="current_jobs" style={{ height: "58vh" }}>
-            <Grid>
-              <Typography variant="h6" style={{ marginBottom: "30px" }}>
-                Current job vacancies :
-              </Typography>
-            </Grid>
+          {currentJob === null && <Header title="Join Our Team" />}
+          <section id="current_jobs" style={{ minHeight: "60vh" }}>
+            {currentJob === null && (
+              <Grid>
+                <Typography variant="h6" style={{ marginBottom: "30px" }}>
+                  Current job vacancies :
+                </Typography>
+              </Grid>
+            )}
             <Grid container justify="center" spacing={3}>
               {isLoading ? (
                 <CircularProgress color="primary" />
+              ) : currentJob !== null ? (
+                <JobDescription
+                  job={currentJob}
+                  setCurrentJob={setCurrentJob}
+                />
               ) : (
-                jobs.map((job) => <JobList key={job.jobId} {...job} />)
+                jobs.map((job) => (
+                  <JobList
+                    key={job.jobId}
+                    job={job}
+                    setCurrentJob={setCurrentJob}
+                  />
+                ))
               )}
             </Grid>
           </section>
