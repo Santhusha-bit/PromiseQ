df30033cbd405d9ec9769fe7839dd343a5c99019
Santhusha <santhushajanana@gmail.com>
2022-07-12 21:43:39 +0530

    translation finished

--- next.config.js
+++ next.config.js
@@ -3,3 +3,15 @@ const nextTranslate = require("next-translate");
 module.exports = nextTranslate({
   trailingSlash: true,
 });
+
+module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/contact": { page: "/contact" },
+    };
+  },
+};
